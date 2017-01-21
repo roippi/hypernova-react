54cf8647ee46adad677f8809baeccae5b1f12eb8
Ben Roberts <beroberts@wayfair.com>
2017-01-21 14:12:29 -0500

    remove inline script, add generated uuid to meta

--- src/index.js
+++ src/index.js
@@ -10,13 +10,40 @@ if (process.env.NODE_ENV === 'production') {
     ReactDOMServer = require('react-dom/server');
 }
 
-import hypernova, { serialize, load } from 'hypernova';
+import hypernova, { load } from 'hypernova';
 
-export const renderReact = (name, component) => hypernova({
+var DATA_KEY = 'hypernova-key';
+var DATA_ID = 'hypernova-id';
+
+// https://gist.github.com/jed/982883
+function uuid() {
+    return (
+        [1e7] +
+        -1e3 +
+        -4e3 +
+        -8e3 +
+        -1e11
+    ).replace(
+        /[018]/g,
+        x => (x ^ Math.random() * 16 >> x / 4).toString(16), // eslint-disable-line no-mixed-operators, no-bitwise, max-len
+    );
+}
+
+function serialize(name, html, data, id) {
+    const key = name.replace(/\W/g, '');
+    return `<div data-${DATA_KEY}="${key}" data-${DATA_ID}="${id}">${html}</div>`;
+}
+
+export const renderReact = (name, component, context) => hypernova({
   server() {
     return (props) => {
       const contents = ReactDOMServer.renderToString(React.createElement(component, props));
-      return serialize(name, contents, props);
+      const generatedUuid = uuid();
+      context.returnMeta = {
+          ...context.returnMeta,
+          uuid: generatedUuid
+      };
+      return serialize(name, contents, props, generatedUuid);
     };
   },
 
